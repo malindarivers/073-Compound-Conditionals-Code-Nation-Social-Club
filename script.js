f9cd8f35599521eab5357e2b9e3f50539957a7e4
malindarivers <43344616+malindarivers@users.noreply.github.com>
2019-04-10 09:35:19 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+// set the correct username and password combination below
+var correctUsername = "melody123";
+var correctPassword = "6520018";
+
+
+$("button").click(function() {
+    var username = $(".username").val();
+    var password = $(".password").val();
+    
+    // WRITE YOUR COMPOUND CONDITIONAL BELOW THIS LINE
+if (username === "melody123" && "6520018" ){
+    $(".message").text("correctUsername!");
+}
+if (password === "6520018" &&
+});
